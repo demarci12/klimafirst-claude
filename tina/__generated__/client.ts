caa7e35dfad83685b91c85f51be94b6e4155ee07
KlimaFirst <info@klimafirst.hu>
2026-03-24 11:11:01 +0100

    fix: update TINA_TOKEN to new project token

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/uih02426/klima-first/tina/__generated__/.cache/1774346211871', url: 'https://content.tinajs.io/2.2/content/c9a364c3-de3f-4eb7-91c1-16bbb844bd71/github/main', token: 'bb7058a4f638697315d05a6c0d9f9925c540d20a', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/uih02426/klima-first/tina/__generated__/.cache/1774347014184', url: 'https://content.tinajs.io/2.2/content/c9a364c3-de3f-4eb7-91c1-16bbb844bd71/github/main', token: 'c784c522bfaa99110123c7ba701e3550fda38e22', queries,  });
 export default client;
   
