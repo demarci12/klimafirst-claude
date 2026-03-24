4cbd4593346a3d7b4dab0019779fdcb7063ac6dd
KlimaFirst <info@klimafirst.hu>
2026-03-24 10:51:46 +0100

    fix: update TinaCloud client ID in generated client

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/uih02426/klima-first/tina/__generated__/.cache/1774172139973', url: 'https://content.tinajs.io/2.2/content/9024a221-2722-48cf-b4bf-e7f26a2bc930/github/main', token: 'bb7058a4f638697315d05a6c0d9f9925c540d20a', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/uih02426/klima-first/tina/__generated__/.cache/1774344006312', url: 'https://content.tinajs.io/2.2/content/c9a364c3-de3f-4eb7-91c1-16bbb844bd71/github/main', token: 'bb7058a4f638697315d05a6c0d9f9925c540d20a', queries,  });
 export default client;
   
